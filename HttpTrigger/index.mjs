c6556b6e8416f9f4b1221d42eaaa8f76fa6783c5
Lars Hvam <larshp@hotmail.com>
2021-12-09 20:03:33 +0100

    add request.body and headers (#5)
    * add request.body
    
    * Update index.mjs
    
    * Update index.mjs
    
    * use rawBody

--- HttpTrigger/index.mjs
+++ HttpTrigger/index.mjs
@@ -8,7 +8,25 @@ export default async function (context, req) {
   abap.console.clear();
   const trig = new abap.Classes['ZCL_HTTP_TRIGGER']();
   await trig.constructor_();
-  const result = await trig.zif_abap_serverless_v1$run({method, query});
+  const request = new abap.types.Structure(
+    {headers: new abap.types.Table(new abap.types.Structure({
+      field: new abap.types.String(),
+      value: new abap.types.String()}),
+      {"withHeader":false,
+       "type":"STANDARD",
+       "isUnique":false,
+       "keyFields":[]}),
+    body: new abap.types.String()});
+  request.get().body.set(req.rawBody || "");
+  for (const h in req.headers) {
+    const add = new abap.types.Structure({
+      field: new abap.types.String(),
+      value: new abap.types.String()});
+    add.get().field.set(h);
+    add.get().field.set(req.headers[h]);
+    request.get().headers.append(add);
+  }
+  const result = await trig.zif_abap_serverless_v1$run({method, query, request});
   console.dir(abap.console.get());
 
   context.res.body = result.get().body.get();
